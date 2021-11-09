28b02cd1fccf7a96c5284db1b20f36bb7bb1d48e
bones <promise.bones@gmail.com>
2021-11-09 22:33:45 +0100

    feat: Create Macros component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,125 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import "./css/App.css";
 import SmileySVG from "./components/svg/smiley";
 import UserSVG from "./components/svg/user";
 import MenuSVG from "./components/svg/menu";
-import ChevronDownSVG from "./components/svg/chevron-down";
 import PinSVG from "./components/svg/pin";
 import { PlusIcon } from "@heroicons/react/solid";
+import { Transition } from "@headlessui/react";
+import { MacrosCol, MacrosRow } from "./components/Macros";
+import cartloopImage from "./assets/cartloop.png";
+
+const sentMessage = {
+  username: "Guy Hawkins — Cartloop",
+  messages: [
+    "Why Netflix shares are down 10%",
+    "Ted Sarandos named co-CEO at Netflix",
+  ],
+};
+
+const eleanorMessage = {
+  username: "Eleanor Pena",
+  messages: [
+    "Immersive chat startups have a very different vision for the future of voice",
+    "What to Watch on Wednesday: Peacock finally hatches with Brave New World, Psych 2, and more",
+  ],
+};
+
+const wadeMessage = {
+  username: "Wade Warren",
+  messages: [
+    "The Calm meditation app is getting its own celebrity-filled HBO Max show",
+    "Apple opens another megastore in China amid William Barr criticism",
+  ],
+};
+
+type SentMessageType = {
+  username: string;
+  messages: string[];
+};
+
+type RecievedMessageType = {
+  username: string;
+  messages: string[];
+};
+
+const SenderMessageCard: React.FC<SentMessageType> = ({
+  username,
+  messages,
+}) => {
+  return (
+    <div className="flex mb-8">
+      <div className="w-11/12 text-right">
+        <div className="mb-1 text-sm">{username}</div>
+
+        {messages.map((message: string, index: number) => (
+          <div
+            key={index}
+            className={`p-4 text-white rounded-b-lg rounded-t-xl bg-primary ${
+              index === 0
+                ? "rounded-b-lg rounded-t-xl mb-2"
+                : index === messages.length - 1
+                ? "rounded-t-lg rounded-b-xl mb-0"
+                : "rounded-lg mb-2"
+            }`}
+          >
+            {message}
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-col items-center justify-end w-1/12">
+        <img src={cartloopImage} className="h-5 w-5" alt="user logo" />
+      </div>
+    </div>
+  );
+};
+
+const RecievedMessageCard: React.FC<RecievedMessageType> = ({
+  username,
+  messages,
+}) => {
+  return (
+    <div className="mb-8">
+      <div className="mb-2 text-sm">{username}</div>
+
+      {messages.map((message: string, index: number) => {
+        return (
+          <Fragment key={index}>
+            {index === 0 ? (
+              <div className="p-4 mb-2 bg-white rounded-b-lg rounded-t-xl">
+                {message}
+              </div>
+            ) : index === messages.length - 1 ? (
+              <div className="flex">
+                <div className="w-5/6 xl:w-11/12">
+                  <div className="p-4 bg-white rounded-t-lg rounded-b-xl">
+                    {message}
+                  </div>
+                </div>
+                <div className="flex justify-between w-1/6 xl:w-1/12 px-2 md:px-3 py-1">
+                  <SmileySVG className="w-5 h-5" />
+                  <UserSVG className="w-5 h-5" />
+                </div>
+              </div>
+            ) : (
+              <div className="p-4 mb-2 bg-white rounded-lg">{message}</div>
+            )}
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+};
 
 function App() {
+  const [message, setMessage] = useState(
+    "Hello Elsa 👋. My name is Jason. What can I do for you today?"
+  );
+
   return (
-    <div className="min-h-screen p-4 bg-white">
-      <main className="">
-        <div className="mb-8 bg-gray-100 rounded-xl">
+    <div className="min-h-screen p-4 bg-white flex">
+      <main className="w-full xl:w-3/4 h-screen">
+        <div className="mb-8 bg-gray-100 rounded-xl overflow-y-scroll h-70vh">
           <div className="flex items-center justify-between px-6 py-4">
             <div className="font-bold text-gray-400">In progress</div>
             <div className="flex items-center">
@@ -21,99 +129,42 @@ function App() {
           <hr className="border" />
 
           <div className="p-4">
-            <div className="mb-8">
-              <div className="mb-1 text-sm">Eleanor Pena</div>
+            <RecievedMessageCard
+              username={eleanorMessage.username}
+              messages={eleanorMessage.messages}
+            />
 
-              <div className="p-4 mb-2 bg-white rounded-b-lg rounded-t-xl">
-                Immersive chat startups have a very different vision for the
-                future of voice
-              </div>
+            <SenderMessageCard
+              username={sentMessage.username}
+              messages={sentMessage.messages}
+            />
 
-              <div className="flex">
-                <div className="w-5/6">
-                  <div className="p-4 bg-white rounded-t-lg rounded-b-xl">
-                    What to Watch on Wednesday: Peacock finally hatches with
-                    Brave New World, Psych 2, and more
-                  </div>
-                </div>
-                <div className="flex justify-between w-1/6 px-3 py-1">
-                  <SmileySVG className="w-5 h-5" />
-                  <UserSVG className="w-5 h-5" />
-                </div>
-              </div>
-            </div>
+            <RecievedMessageCard
+              username={wadeMessage.username}
+              messages={wadeMessage.messages}
+            />
 
-            <div className="flex mb-8">
-              <div className="w-11/12 text-right">
-                <div className="mb-1 text-sm">Guy Hawkins — Cartloop</div>
-                <div className="p-4 mb-2 text-white rounded-b-lg rounded-t-xl bg-primary">
-                  Why Netflix shares are down 10%
-                </div>
-                <div className="p-4 text-white rounded-t-lg rounded-b-xl bg-primary">
-                  Ted Sarandos named co-CEO at Netflix
-                </div>
-              </div>
-              <div className="flex flex-col items-center justify-end w-1/12">
-                <SmileySVG className="w-5 h-5 p-1 text-white rounded-full bg-primary" />
-              </div>
-            </div>
-
-            <div className="mb-8">
-              <div className="mb-1 text-sm">Wade Warren</div>
-
-              <div className="p-4 mb-2 bg-white rounded-b-lg rounded-t-xl">
-                The Calm meditation app is getting its own celebrity-filled HBO
-                Max show
-              </div>
-
-              <div className="flex">
-                <div className="w-5/6">
-                  <div className="p-4 bg-white rounded-t-lg rounded-b-xl">
-                    Apple opens another megastore in China amid William Barr
-                    criticism
-                  </div>
-                </div>
-                <div className="flex justify-between w-1/6 px-3 py-1">
-                  <SmileySVG className="w-5 h-5" />
-                  <UserSVG className="w-5 h-5" />
-                </div>
-              </div>
-            </div>
+            <SenderMessageCard
+              username={sentMessage.username}
+              messages={sentMessage.messages}
+            />
           </div>
         </div>
 
         <div className="p-2 mb-8 bg-gray-100 rounded-xl">
-          <div className="px-3 py-4 mb-4 bg-white rounded-lg shadow-md">
-            <div className="flex items-center justify-between mb-4">
-              <div className="font-bold">Macros</div>
-              <div>
-                <ChevronDownSVG className="w-5 h-5" />
-              </div>
-            </div>
-
-            {/* <div className="flex flex-no-wrap justify-between overflow-x-scroll md:flex-wrap md:overflow-x-hidden"> */}
-            <div className="flex flex-no-wrap justify-between overflow-x-scroll pt-1 pb-4">
-              <div className="min-w-3/4 p-4 bg-gray-100 rounded-lg mr-4 border-4 border-transparent border-solid hover:border-primary">
-                Immersive chat startups have a very different vision for the
-                future of voice
-              </div>
-
-              <div className="min-w-3/4 p-4 bg-gray-100 rounded-md mr-4">
-                Hello <b>%user%</b>👋. My name is <b>%expert%</b>. What can I do
-                for you today?
-              </div>
-
-              <div className="min-w-3/4 p-4 bg-gray-100 rounded-md mr-4">
-                Apple opens another megastore in China amid William Barr
-                criticism
-              </div>
-            </div>
+          <div className="xl:hidden">
+            <MacrosRow />
           </div>
 
           <div className="px-6 py-4 bg-white rounded-lg shadow-md">
             <div className="mb-4">
-              Hello <b>Elsa</b> 👋. My name is <b>Jason</b>. What can I do for
-              you today?
+              <textarea
+                rows={2}
+                className="w-full focus:outline-none appearance-none"
+                placeholder="Type a message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
             </div>
 
             <hr className="" />
@@ -141,6 +192,10 @@ function App() {
           </div>
         </div>
       </main>
+
+      <div className="hidden xl:flex w-1/4">
+        <MacrosCol />
+      </div>
     </div>
   );
 }
---
+++ src/components/Macros.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import ChevronDownSVG from "./svg/chevron-down";
+
+const macros = [
+  "Immersive chat startups have a very different vision for the future of voice",
+  "Hello user 👋. My name is expert. What can I do for you today?",
+  "Apple opens another megastore in China amid William Barr criticism",
+];
+
+type MacrosType = {};
+
+type MacroCardType = {
+  text: string;
+};
+
+export const MacrosRow: React.FC<MacrosType> = () => {
+  const [macrosOpen, setMacrosOpen] = useState(true);
+
+  const MacrosCard: React.FC<MacroCardType> = ({ text }) => (
+    <div className="min-w-3/4 p-4 bg-gray-100 rounded-lg mr-4 border-4 border-transparent border-solid hover:border-primary cursor-pointer">
+      {text}
+    </div>
+  );
+
+  return (
+    <div className="px-3 py-4 mb-4 bg-white rounded-lg shadow-md">
+      <div className="flex items-center justify-between mb-4">
+        <div className="font-bold">Macros</div>
+        <button type="button" onClick={(e) => setMacrosOpen(!macrosOpen)}>
+          <ChevronDownSVG
+            className={`w-5 h-5 transform ${
+              macrosOpen ? "rotate-180" : "rotate-0"
+            }`}
+          />
+        </button>
+      </div>
+
+      {/* <div className="flex flex-no-wrap justify-between overflow-x-scroll md:flex-wrap md:overflow-x-hidden"> */}
+      {macrosOpen ? (
+        <div className="flex pt-1 pb-4 flex-no-wrap justify-between overflow-x-scroll">
+          {macros.map((text, index) => (
+            <MacrosCard key={index} text={text} />
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export const MacrosCol: React.FC<MacrosType> = () => {
+  const [macrosOpen, setMacrosOpen] = useState(true);
+
+  const MacrosCard: React.FC<MacroCardType> = ({ text }) => (
+    <div className="p-4 bg-white rounded-lg mb-2 shadow-md border-4 border-transparent border-solid hover:border-primary cursor-pointer">
+      {text}
+    </div>
+  );
+
+  return (
+    <div className="px-3 py-4 bg-gray-100 rounded-lg ml-4 w-full">
+      <div className="flex items-center justify-between mb-4">
+        <div className="font-bold">Macros</div>
+        <button type="button" onClick={(e) => setMacrosOpen(!macrosOpen)}>
+          <ChevronDownSVG
+            className={`w-5 h-5 transform ${
+              macrosOpen ? "rotate-180" : "rotate-0"
+            }`}
+          />
+        </button>
+      </div>
+
+      {macrosOpen ? (
+        <div className="flex flex-col pt-1 pb-4">
+          {macros.map((text, index) => (
+            <MacrosCard key={index} text={text} />
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
+};
