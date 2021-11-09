a182ef89efe2bb7077410fb26ec7a1469b834ba8
bones <promise.bones@gmail.com>
2021-11-09 22:33:30 +0100

    feat: Extend utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,17 @@ module.exports = {
         85: "85%",
         full: "100%",
       },
+      height: {
+        "10vh": "10vh",
+        "20vh": "20vh",
+        "30vh": "30vh",
+        "40vh": "40vh",
+        "50vh": "50vh",
+        "60vh": "60vh",
+        "70vh": "70vh",
+        "80vh": "80vh",
+        "90vh": "90vh",
+      },
     },
   },
   variants: {
