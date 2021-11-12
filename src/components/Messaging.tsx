ef5c1c1e181c7e5a9564b20ae670852d39a4382b
bones <promise.bones@gmail.com>
2021-11-12 01:58:06 +0100

    refactor

--- src/components/Messaging.tsx
+++ src/components/Messaging.tsx
@@ -20,12 +20,12 @@ export const OutBoundMessageCard: React.FC<OutBoundMessageType> = ({
   return (
     <div className="flex mb-8">
       <div className="w-11/12 text-right">
-        <div className="mb-1 text-sm">{username}</div>
+        <div className="mb-1 text-sm text-cl-black">{username}</div>
 
         {messages.map((message: string, index: number) => (
           <div
             key={index}
-            className={`p-4 text-white rounded-b-lg rounded-t-xl bg-primary ${
+            className={`px-4 py-2 text-white rounded-b-lg rounded-t-xl bg-primary ${
               index === 0
                 ? "rounded-b-lg rounded-t-xl mb-2"
                 : index === messages.length - 1
@@ -38,7 +38,7 @@ export const OutBoundMessageCard: React.FC<OutBoundMessageType> = ({
         ))}
       </div>
       <div className="flex flex-col items-center justify-end w-1/12">
-        <img src={cartloopImage} className="h-5 w-5" alt="user logo" />
+        <img src={cartloopImage} className="w-5 h-5" alt="user logo" />
       </div>
     </div>
   );
@@ -48,31 +48,42 @@ export const InBoundMessageCard: React.FC<InBoundMessageType> = ({
   username,
   messages,
 }) => {
+  type MessageCardType = {
+    message: string;
+    className: string;
+  };
+
+  const MessageBox: React.FC<MessageCardType> = ({ message, className }) => (
+    <div className={`px-4 py-2 bg-white ${className}`}>{message}</div>
+  );
+
   return (
     <div className="mb-8">
-      <div className="mb-2 text-sm">{username}</div>
+      <div className="mb-2 text-sm text-cl-black">{username}</div>
 
       {messages.map((message: string, index: number) => {
         return (
           <Fragment key={index}>
             {index === 0 ? (
-              <div className="p-4 mb-2 bg-white rounded-b-lg rounded-t-xl">
-                {message}
-              </div>
+              <MessageBox
+                message={message}
+                className="mb-2 rounded-b-lg rounded-t-xl"
+              />
             ) : index === messages.length - 1 ? (
               <div className="flex">
                 <div className="w-5/6 xl:w-11/12">
-                  <div className="p-4 bg-white rounded-t-lg rounded-b-xl">
-                    {message}
-                  </div>
+                  <MessageBox
+                    message={message}
+                    className="rounded-t-lg rounded-b-xl"
+                  />
                 </div>
-                <div className="flex justify-between w-1/6 xl:w-1/12 px-2 md:px-3 py-1">
+                <div className="flex justify-between w-1/6 px-2 py-1 xl:w-1/12 md:px-3">
                   <SmileySVG className="w-5 h-5" />
                   <UserSVG className="w-5 h-5" />
                 </div>
               </div>
             ) : (
-              <div className="p-4 mb-2 bg-white rounded-lg">{message}</div>
+              <MessageBox message={message} className="mb-2 rounded-lg" />
             )}
           </Fragment>
         );
